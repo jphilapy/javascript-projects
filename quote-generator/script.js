3a2fff3c2248e85a87a8f920246b9e5b621831ec
Jeff Philapy <jeff@altahost.com>
2020-12-05 19:55:15 -0500

    added scripting to connect to api

---
+++ quote-generator/script.js
@@ -0,0 +1,14 @@
+async function getQUote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.log('oops, no quote!', error)
+    }
+}
+
+getQuote();
